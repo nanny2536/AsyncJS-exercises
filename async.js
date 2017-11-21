ebebddc00c1e19df9b34b5c6049199c81d6aaf40
nanny2536 <ini3nany_nan@hotmail.com>
2017-11-21 10:31:53 +0700

    test

--- async.js
+++ async.js
@@ -1,7 +1,9 @@
 const request = require('request');
 
 const test = () => {
-  
+  request('http://www.google.com',function (error, response, body){
+      console.log('response');
+  });
 }
 //START OF PROGRAM
 console.log("Hitting google");
